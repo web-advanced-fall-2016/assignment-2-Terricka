704b2f6d54839f5aa19408b0003b81149391f09c
Terricka <johnt626@newschool.edu>
2016-11-09 19:50:54 -0500

    Updated

--- main.js
+++ main.js
@@ -6,17 +6,17 @@ var moreButton = document.getElementById('block1');
 // console.log(moreButton);
 
 var container = document.getElementById("container");
+var blocks = document.getElementsByClassName('block');
 
 
 window.onload=function(){
 	
-		
 			
 			$.ajax({
 				method: "GET",
 				url: url + `/students`
 			}).done(function(response){
-				console.log(response);
+				// console.log(response);
 				for(var i = 0; i < response.length; i++){
 					var count = i;
 
@@ -25,18 +25,22 @@ window.onload=function(){
 							method: "GET",
 							url: url+'/students/'+response[i].id
 						}).done(function(response){
-								console.log(response);
+								// console.log(response);
 
 								var div = document.createElement('div');
-								div.id = count;
+								// div.id = count;
 								div.className = 'block';
 								container.appendChild(div);
 
+								div.dataset.id = response.id;
+
 								var link = document.createElement('a');
-								link.id = 'profile-' + [i];
+								link.id = 'profile-' + response.id;
 								link.href = '#';
 
 								div.appendChild(link);
+								
+
 
 								
 								var image = document.createElement('img');
@@ -50,29 +54,47 @@ window.onload=function(){
 
 								link.appendChild(overlay);
 
-								// overlay.html()
+								div.addEventListener('mouseenter', function(evnt){
+									var thisId = $(this).attr("data-id");
+									var overlayed = $(this).find('.overlay');
+								//this.querySelector('.overlay').innerText = "true"
+									// console.log(thisId);
+
+									var p = document.createElement("p");
+									overlay.appendChild(p);
+									// console.log("works");
+									
+
+									$.ajax({
+										method: "GET",
+										url: url +`/students/`+evnt.target.dataset.id
+									}).done(function(response){
+										// console.log("yes")
+										overlay.innerHTML += '<h1>' + response.first_name + ' ' + response.last_name + '</h1>';
+										overlay.innerHTML += '<br><p>'
+										overlay.innerHTML += response.email;
+										overlay.innerHTML += '</p><br><p>';
+										overlay.innerHTML += response.excerpt;
+										overlay.innerHTML += '</p>'
+										
+									})
+								});
+
+								div.addEventListener('mouseleave', function(evnt){
+									overlay.innerHTML = '';
+								
+								})
+
 
 						})
 
 
-						var blocks = document.getElementsByClassName('block');
-			
-
-						
-
-
 				}
-
-
-
 
 				console.log("success");
 			});
 
-
 			
-		
-		
-
+			
 
 };
